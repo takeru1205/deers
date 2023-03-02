93237e5f5d28a2d60cb17aa4d5f4c6734057e175
takeru1205 <takeru0639418@gmail.com>
2023-03-02 21:36:28 +0900

    [update] simple graph

--- src/graph.rs
+++ src/graph.rs
@@ -1,22 +1,59 @@
-use pyo3::types::PyBytes;
-use pyo3::{
-    exceptions::{PyTypeError, PyValueError},
-    prelude::*,
-    types::{PyTuple, PyType},
-};
+use pyo3::{exceptions::PyValueError, prelude::*};
 use std::collections::HashMap;
 
-pub struct node {
-    node: i32,
-}
-
 #[pyclass]
-#[derive(Clone)]
 pub struct Graph {
-    graph: HashMap,
-    _node: HashMap,
-    _adj: HashMap,
+    graph: HashMap<i32, HashMap<String, i32>>,
+    _node: HashMap<i32, HashMap<String, i32>>,
+    _adj: HashMap<i32, HashMap<String, i32>>,
     number_of_nodes: i32,
     number_of_edges: i32,
-    name: &str,
+    name: String,
+}
+
+#[pymethods]
+impl Graph {
+    #[new]
+    fn new() -> Self {
+        Graph {
+            graph: HashMap::new(),
+            _node: HashMap::new(),
+            _adj: HashMap::new(),
+            number_of_nodes: 0,
+            number_of_edges: 0,
+            name: String::new(),
+        }
+    }
+
+    fn add_node(&mut self, node_for_adding: Option<i32>) -> PyResult<()> {
+        if !self._node.contains_key(&node_for_adding.unwrap()) {
+            if node_for_adding.is_none() {
+                return Err(PyValueError::new_err("Value Error: None cannot be a node"));
+            }
+            self._adj.insert(node_for_adding.unwrap(), HashMap::new());
+            self._node.insert(node_for_adding.unwrap(), HashMap::new());
+        };
+        Ok(())
+    }
+
+    /*
+    fn __str__<'p>(&self, py: Python<'p>) -> PyResult<PyString> {
+        let mut print_statement = String::new();
+        for (key, value) in &self._node {
+            println!("{}: {:?}", key, value);
+            print_statement = print_statement + &format!("{}: {:?}", key, value);
+        }
+        Ok(PyString::new(py, &print_statement))
+    }
+    */
+    fn str(&self) -> PyResult<()> {
+        for (key, value) in &self._node {
+            println!("{}: {:?}", key, value);
+        }
+        Ok(())
+    }
+
+    fn __len__(&self) -> usize {
+        return self._node.len() as usize;
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,9 @@
 use pyo3::prelude::*;
+mod graph;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust.
 #[pymodule]
-fn deers(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+#[pyo3(name = "deers")]
+fn my_module(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_class::<graph::Graph>()?;
     Ok(())
 }
