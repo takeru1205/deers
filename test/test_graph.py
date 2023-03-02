0d1473a97ead9f7bcd4dd20d8d8cd307cdf87441
takeru1205 <takeru0639418@gmail.com>
2023-03-02 23:58:00 +0900

    [update] add test case for name

--- test/test_graph.py
+++ test/test_graph.py
@@ -6,3 +6,18 @@ def test_graph():
     g.add_node(1)
 
     print(g.str())
+
+
+def test_add_node():
+    g = deers.Graph()
+    for i in range(10):
+        g.add_node(i)
+
+    print(g.str())
+
+
+def test_name():
+    name = "test"
+    named_g = deers.Graph(name=name)
+    named_g.add_node(1)
+    assert name == named_g.name
